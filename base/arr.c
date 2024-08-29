f3f06ba7f3d4abfbff74bc503623ffa357b7036f
Justin-12138 <lz12138@mails.guet.edu.cn>
2024-08-29 14:36:34 +0800

    add ptr_real swap

--- base/arr.c
+++ base/arr.c
@@ -3,53 +3,42 @@
 #include<stdbool.h>
 #include<string.h>
 
-#define maxsize 100
-typedef struct string
-{
-    char data[maxsize];
-    int length;
-}str;
-
-void initstring(str *str){
-str->length=0;
-}
-void crate(str*s,char){
-
-}
-
-
-
-
 int main(){
-
-// char only '' %c
-// int arr[4]={'a','b','c','2'};
-
-// for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++)
-// {
-//     printf("arr[%d]:%c\n",i+1,arr[i]);
-// }
-
-//string char[]={"a44bc"} as a matter of fact \0 at the end of the string,so the length is 6
-// the type of whole string "a44bc" use the %s!!!
-
-// char str[]="a44bc";
-// printf("str:%s,length:%ld\n",str,sizeof(str)/sizeof(str[0]));
-
-//if you asign the length char str1[4]="abcd",it wont't create the \0 as singal of end!
-//at the same time,the type of every single letter must use the %c!!!
-
-// for (int j = 0; j < sizeof(str)/sizeof(str[0]); j++)
-// {
-//     printf("str[%d]:%c\n",j+1,str[j]);
-// }
-// char str1[4]={"1221"};
-// for (int i = 0; i < sizeof(str1)/sizeof(str1[0]); i++)
-// {
-//     printf("str1[%d]:%c\n",i+1,str1[i]);
-// }
+    
 
 
+
+    // int *p=&arr[3];
+    // printf("addr:%p\n",p);
+    // printf("the value of %p:%d\n",p,arr[3]);
+    // *p=3;
+    // printf("the value of %p:%d\n",p,arr[3]);
+    // char only '' %c
+    // int arr[4]={'a','b','c','2'};
+
+    // for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++)
+    // {
+    //     printf("arr[%d]:%c\n",i+1,arr[i]);
+    // }
+
+    //string char[]={"a44bc"} as a matter of fact \0 at the end of the string,so the length is 6
+    // the type of whole string "a44bc" use the %s!!!
+
+    // char str[]="a44bc";
+    // printf("str:%s,length:%ld\n",str,sizeof(str)/sizeof(str[0]));
+
+    //if you asign the length char str1[4]="abcd",it wont't create the \0 as singal of end!
+    //at the same time,the type of every single letter must use the %c!!!
+
+    // for (int j = 0; j < sizeof(str)/sizeof(str[0]); j++)
+    // {
+    //     printf("str[%d]:%c\n",j+1,str[j]);
+    // }
+    // char str1[4]={"1221"};
+    // for (int i = 0; i < sizeof(str1)/sizeof(str1[0]); i++)
+    // {
+    //     printf("str1[%d]:%c\n",i+1,str1[i]);
+    // }
 
 
     return 0;
--- base/arry_prt.c
+++ base/arry_prt.c
@@ -10,6 +10,14 @@ void ptf(int *arr,int size){
     printf("-----------------------------\n");
 }
 
+void swap(int *num1,int *num2){
+	int swap;
+    swap=*num1;
+	*num1=*num2;
+	*num2=swap;
+}
+
+
 int *merge(int *arr1,int size1,int *arr2,int size2){
     int size3=size1+size2;
     int *arr3=(int*)malloc(sizeof(int)*size3);
@@ -27,16 +35,39 @@ int *merge(int *arr1,int size1,int *arr2,int size2){
 }
 
 
-int  *desrank(int *arr,int size){
-    for (int i = 0; i < size-1; i++)
+// int  *desrank(int *arr,int size){
+//     for (int i = 0; i < size-1; i++)
+//     {
+//         for (int j = 0; j < size-1-i; j++)
+//         {
+//             swap(&arr[j],&arr[j+1]);
+//         }
+//     }
+//     return arr;
+// }
+
+// int *opt_desrank(int *arr,int size){
+//     int size_type=size%2;
+//     if (size_type==0)
+//     {
+//         for (int i = 0; i < size/2; i++)
+//         {
+//             swap(&arr[i],&arr[size-i-1]);
+//         }
+//     }
+//     else{
+//         for (int i = 0; i < (size/2)+1; i++)
+//         {
+//             swap(&arr[i],&arr[size-i-1]);
+//         }
+//     }
+//     return arr;
+// }
+
+int * opt_desrank(int *arr,int size){
+    for (int i = 0; i < size/2; i++)
     {
-        int swap;
-        for (int j = 0; j < size-1-i; j++)
-        {
-            swap=arr[j];
-            arr[j]=arr[j+1];
-            arr[j+1]=swap;
-        }
+        swap(&arr[i],&arr[size-i-1]);
     }
     return arr;
 }
@@ -47,22 +78,17 @@ for (int i = 0; i < size-1; i++)
 {
     for (int j = 0; j < size -1; j++)
     {
-        int swap;
         if (ascend)
         {
             if (arr[j]>arr[j+1])
             {
-                swap=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=swap;
+                swap(&arr[j],&arr[j+1]);
             }
         }
         else{
             if (arr[j]<arr[j+1])
             {
-                swap=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=swap;
+                swap(&arr[j],&arr[j+1]);
 
             }
         }
@@ -75,14 +101,15 @@ return arr;
 
 }
 
+
 int main(){
     int arr1[]={11,2,3};
-    int arr2[]={44,6,6,2};
+    int arr2[]={44,6,6,4};
     int size1=sizeof(arr1)/sizeof(arr1[0]);
     int size2=sizeof(arr2)/sizeof(arr2[0]);
     int *arr3=merge(arr1,size1,arr2,size2);
     ptf(arr3,7);
-    int *arr4=desrank(arr3,7);
+    int *arr4=opt_desrank(arr3,7);
     ptf(arr4,7);
 
     int *arr5=bublerank(arr3,7,false);
