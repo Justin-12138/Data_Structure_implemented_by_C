1082ccf8bf01e0bc969edf8e61bee68381dedcfe
Justin-12138 <lz12138@mails.guet.edu.cn>
2024-09-01 23:42:28 +0800

    add tree

---
+++ tree/tree_dp.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+
+#define maxsize 100
+
+typedef struct treenode
+{
+    int data;
+    int parent;
+}treenode;
+
+typedef struct tree
+{
+    treenode data[maxsize];
+    int count;
+}tree;
+
+
+void inittree(tree*t){
+t->count=0;
+}
+
+
+
+
+
+
+
+
+
+int main(){
+
+
+
+    return 0;
+}
